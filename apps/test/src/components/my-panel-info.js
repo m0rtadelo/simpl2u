221c0f06d40fc5b449473828c4f8a8a06bb3f419
Ricard Fíguls Mateu <ricard@gmail.com>
2025-04-11 00:43:25 +0200

    WIP

--- apps/test/src/components/my-panel-info.js
+++ apps/test/src/components/my-panel-info.js
@@ -5,8 +5,25 @@ export class MyPanelInfo extends MyPanel {
         return `
         <div>Name: ${state.name || ""}</div>
         <div>Surname: ${state.surname || ""}</div>
-        <my-button></my-button>
+        <my-button id="myButton""></my-button>
         `
     }
+
+    button() {
+        console.log('clicked!!');
+        alert('clicked');
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+        Promise.resolve().then(() => {
+          const btn = this.querySelector('#myButton');
+          if (btn) {
+            btn.removeEventListener('click', this.buttonBound);
+            this.buttonBound = this.button.bind(this);
+            btn.addEventListener('click', this.buttonBound);
+          }
+        });
+      }    
 }
 customElements.define('my-panel-info', MyPanelInfo);
